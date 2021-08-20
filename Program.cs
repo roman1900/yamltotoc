8d4967921906f5edfe4596721c24ff1eba375d78
Matt Marschall <mmarschall@redmars.com.au>
2021-08-20 11:45:04 +1000

    Implemented command line parameter processing

--- Program.cs
+++ Program.cs
@@ -8,8 +8,54 @@ namespace yamltotoc
 {
     class Program
     {
+        enum parameters {
+            append,
+            output,
+            undefined
+        }
         static void Main(string[] args)
         {
+            bool append = false;
+            string output = "readme.md";
+            parameters currentArg = parameters.undefined;
+            if (args.Length >0)
+            {
+                foreach (string arg in args)
+                {
+                    if (arg.Substring(0,1) == "-")
+                    {
+                        //Parameter
+                        switch (arg)
+                        {
+                            case "-a":
+                            case "--append":
+                                append = true;
+                                break;
+                            case "-o":
+                            case "--output":
+                                currentArg = parameters.output;
+                                break;
+                            default:
+                                throw(new System.Exception($"Unknown parameter supplied {arg}"));
+                        } 
+                        
+                    }
+                    else 
+                    {
+                        //Value
+                        switch (currentArg) 
+                        {
+                            case parameters.output:
+                                output = arg;
+                                break;
+                            case parameters.undefined:
+                            default:
+                                throw(new System.Exception($"Unknown bare word parameter supplied {arg} are you missing a switch?"));
+                        }
+                        currentArg = parameters.undefined;
+                    }
+                }
+            }
             var deserializer = new DeserializerBuilder()
                 .IgnoreUnmatchedProperties()
                 .Build();
@@ -18,7 +64,7 @@ namespace yamltotoc
             var mdfiles = Directory.GetFiles(".","*.md");
             foreach (string doc in mdfiles)
             {
-                if (doc != @".\readme.md") { //we will output the readme.md
+                if (doc != @".\"+output) { //do not include the output file
                     var content = File.ReadAllLines(doc);
                     string yaml = "";
                     if (content[0].Replace(" ","")  == "---") { //yaml content in doc
@@ -46,7 +92,10 @@ namespace yamltotoc
                             + System.Environment.NewLine;
                 }
                 //TODO: #2 Implement command line parameter for alternate output file
-                File.WriteAllText("readme.md",toc);
+                if (append)
+                    File.AppendAllText(output,toc);
+                else
+                    File.WriteAllText(output,toc);
             }
         }   
             
